f6c977e2ad13acee80131fcaddd94eef32fd42fe
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2024-10-07 18:01:27 +0500

    FirstHomework

---
+++ SuperheroHomework/Superhero.cs
@@ -0,0 +1,20 @@
+﻿namespace SuperheroHomework;
+
+public class Superhero
+{
+    private static Superhero superman;
+    private static Superhero batman;
+
+    public string Name { get; private set; }
+
+    private Superhero(string name) 
+    {
+        Name = name;
+    }
+
+    public static Superhero CreateSuperman() => superman ??= new Superhero("Супермен");
+
+    public static Superhero CreateBatman() => batman ??= new Superhero("Бэтмен");
+ }
+
+
